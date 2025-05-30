26d8368fab9088d9ecd919b9223a73c94dc57396
Kh4tib <khatib100601@gmail.com>
2025-05-30 16:06:44 +0200

    Fix: netlify adapter and root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <section class="min-vh-90 d-flex flex-column justify-content-center align-items-center text-white bg-cover w-100">
   <div class="bg-dark bg-opacity-75 p-5 rounded text-center">
-    <h1 class="display-4 mb-3"> Willkommen bei Movie-DB</h1>
+    <h1 class="display-4 mb-3">Willkommen bei Movie-DB</h1>
     <p class="lead mb-4">Verwalte deine Lieblingsfilme mit Stil.</p>
-    <a href="/movies" class="btn btn-primary btn-lg"> Zu den Filmen</a>
+    <a href="/movies" class="btn btn-primary btn-lg">Zu den Filmen</a>
   </div>
 </section>
 
@@ -10,12 +10,14 @@
   :global(body) {
     margin: 0;
     background-color: #000;
+    font-family: system-ui, sans-serif;
   }
 
   .bg-cover {
-    background-image: url('/bg.jpg');
+    background-image: url('/bg.jpg'); /* ✅ wichtig: muss in /static/bg.jpg liegen */
     background-size: cover;
     background-position: center;
+    background-repeat: no-repeat;
   }
 
   .min-vh-90 {
